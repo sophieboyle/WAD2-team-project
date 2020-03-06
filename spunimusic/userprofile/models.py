968552e869047061b09b154b2a26bc1f5b38273d
sophieboyle <sophieboyle4028@gmail.com>
2020-03-06 20:26:25 +0000

    Linked UserProfile model to User

--- spunimusic/userprofile/models.py
+++ spunimusic/userprofile/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 from spunimusic.spuni.models import Song
+from django.contrib.auth.models import User
 
 """
     @brief User model
-    @field string name: User's username
-    @field email email: User's email
     @field url photo: Link to user's profile picture
     @field (FK) Song upvotedSongs: Songs upvoted by the user
     Note: ID is automatically implemented by django
+    Name and email implemented by User model
 """
-class User(models.Model):
-    NAME_MAX_LENGTH = 128
-    EMAIL_MAX_LENGTH = 254
+class UserProfile(models.Model):
     PHOTO_MAX_LENGTH = 200
     
-    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
-    email = models.EmailField(max_length=EMAIL_MAX_LENGTH)
+    # Link currently defined model to user model for auth stuff
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
     photo = models.URLField(max_length=PHOTO_MAX_LENGTH)
     upvotedSongs = models.ForeignKey(Song)
