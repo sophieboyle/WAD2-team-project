d89f56154d99495e57aac5a6b44f17e532921cea
Fraser Paterson <2315720p@student.gla.ac.uk>
2020-03-08 16:40:43 +0000

    updated so http://localhost:8000/ works, and added medial folders and directories to project

--- spunimusic/spunimusic/urls.py
+++ spunimusic/spunimusic/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from spuni import views
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('social/', include('social_django.urls')),
     path('', include('django.contrib.auth.urls')),
