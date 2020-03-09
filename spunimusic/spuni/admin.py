61d98029e88341b57a9e31980cacffb16f6ea239
sophieboyle <sophieboyle4028@gmail.com>
2020-03-09 13:04:26 +0000

    Added form models for user and userprofile, also added userprofile model to admin view.

--- spunimusic/spuni/admin.py
+++ spunimusic/spuni/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from spuni.models import Song
+from spuni.models import Song, UserProfile
 
 class SongAdmin(admin.ModelAdmin):
     list_display = ('name', 'upvotes', 'artist')
 
+
 admin.site.register(Song, SongAdmin)
+admin.site.register(UserProfile)
